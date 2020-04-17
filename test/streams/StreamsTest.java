40202193cfc85ad763c6610411652f10fbd71223
lev94 <62993371+lev94@users.noreply.github.com>
2020-04-17 19:25:52 +0200

    Update StreamsTest.java

--- test/streams/StreamsTest.java
+++ test/streams/StreamsTest.java
@@ -2,34 +2,40 @@ package streams;
 
 import org.junit.Assert;
 import org.junit.Test;
-
+import java.beans.PersistenceDelegate;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public class StreamsTest {
-    long time;
 
     @Test
-    public void GutTest1() throws Exception {
+    public void gutTest1() throws Exception {
         Streams data = new StreamsImpl();
 
-        float[] values = new float[2];
+        long time = System.currentTimeMillis();
+        float[] v = new float[2];
+        v[0] = (float) 3.5;
+        v[1] = (float) 8.9;
 
-        double daten = data.saveData(time = System.currentTimeMillis(),new float[]{values[0] = (float) 3.5,values[1] = (float) 8.9});
-        Assert.assertArrayEquals(new float[]{(float) 3.5, (float) 8.9},new float[]{values[0], values[1]}, 0);
-        Assert.assertEquals(this.time, time);
+        double daten = data.saveData(time,v);
+        Assert.assertArrayEquals(v, v, 0); //vor 0 steht "delta:"
+        Assert.assertEquals(time, time);
     }
 
-    @Test(expected = Exception.class)
-    public void SchlechtTest1() throws Exception {
+    //@Test(expected = Exception.class)
+    @Test(expected = AssertionError.class) //AssertionError.class statt Exception.class?
+    public void schelchtTest1() throws Exception {
         Streams data = new StreamsImpl();
 
-        float[] values = new float[2];
+        long time = System.currentTimeMillis();
+        float[] v = new float[2];
+        v[0] = (float) 3.5;
+        v[1] = (float) 8.9;
 
-        double daten = data.saveData(time = System.currentTimeMillis(), new float[]{values[0] = (float) 3.5,values[1] = (float) 8.9});
-        Assert.assertArrayEquals(new float[]{(float) 3.5, (float) 10.9},new float[]{values[0], values[1]}, 0);
-        Assert.assertEquals(this.time-100, time);
+        double daten = data.saveData(time,v);
+        Assert.assertArrayEquals(v, v, 0);
+        Assert.assertEquals(time-2000, time);
     }
 }
