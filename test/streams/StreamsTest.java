ffe12656b5bd76c7441989fddc04b4dda14cd7ef
lev94 <62993371+lev94@users.noreply.github.com>
2020-04-18 12:20:50 +0200

    Update StreamsTest.java

--- test/streams/StreamsTest.java
+++ test/streams/StreamsTest.java
@@ -2,6 +2,7 @@ package streams;
 
 import org.junit.Assert;
 import org.junit.Test;
+
 import java.beans.PersistenceDelegate;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -24,18 +25,20 @@ public class StreamsTest {
         Assert.assertEquals(time, time);
     }
 
-    //@Test(expected = Exception.class)
-    @Test(expected = AssertionError.class) //AssertionError.class statt Exception.class?
+    @Test
     public void schelchtTest1() throws Exception {
         Streams data = new StreamsImpl();
+        try {
+            long time = System.currentTimeMillis();
+            float[] v = new float[Integer.MAX_VALUE+1];
+            for(long i = 0; i <= v.length; i++) {
+                v[(int) i] = (float) 3.5;
+            }
 
-        long time = System.currentTimeMillis();
-        float[] v = new float[2];
-        v[0] = (float) 3.5;
-        v[1] = (float) 8.9;
+            int daten = (int) data.saveData(time,v);
+            Assert.fail();
+        } catch (Exception e) {
 
-        double daten = data.saveData(time,v);
-        Assert.assertArrayEquals(v, v, 0);
-        Assert.assertEquals(time-2000, time);
+        }
     }
 }
