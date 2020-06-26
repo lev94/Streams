595e5ee0363c6f7ff28caf643c540f0fc026624b
lev94 <62993371+lev94@users.noreply.github.com>
2020-06-26 14:57:44 +0200

    Delete SchiffeVersenkenStatus.java

--- src/schiffeVersenken/SchiffeVersenkenStatus.java
+++
@@ -1,5 +0,0 @@
-package schiffeVersenken;
-
-public enum SchiffeVersenkenStatus {
-    START, WON, LOST, DICE_SENT, VERSENKEN_EMPFANGEN, BESTAETIGEN_SENDEN, VERSENKEN_SENDEN, BESTAETIGEN_EMPFANGEN
-}
